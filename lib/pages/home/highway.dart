4140ced6c62afb037fc5ee4bc053bf8e85c20b9d
Lixing <lixing.sun@thoughtworks.com>
2022-01-01 23:13:06 +0800

    Enable deletion of in progress item

--- lib/pages/home/highway.dart
+++ lib/pages/home/highway.dart
@@ -1,13 +1,15 @@
-
 import 'package:flutter/material.dart';
 import 'package:dellery_app/components/progress_bar.dart';
 
 import '../../store.dart';
 
 class HighwayContent extends StatefulWidget {
-  const HighwayContent({Key? key, required this.ongoingList}) : super(key: key);
+  const HighwayContent(
+      {Key? key, required this.ongoingList, required this.localStorage})
+      : super(key: key);
 
   final List<OngoingItem> ongoingList;
+  final LocalStorage localStorage;
 
   @override
   _HighwayContentState createState() => _HighwayContentState();
@@ -19,9 +21,42 @@ class _HighwayContentState extends State<HighwayContent> {
         itemCount: widget.ongoingList.length,
         itemBuilder: (context, index) {
           return ProgressBar(
-              title: widget.ongoingList[index].title,
-              percent: widget.ongoingList[index].percent,
-              icon: Icon(typeMap[widget.ongoingList[index].type]));
+            title: widget.ongoingList[index].title,
+            percent: widget.ongoingList[index].percent,
+            icon: Icon(typeMap[widget.ongoingList[index].type]),
+            onEdit: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const AlertDialog(title: Text("Edit"));
+                  });
+            },
+            onDelete: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Delete"),
+                      content: Text(
+                          'Confirm to delete the ${index < widget.ongoingList.length ? widget.ongoingList[index].title : ""}'),
+                      actions: [
+                        TextButton(
+                            child: const Text("DELETE"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              widget.localStorage.deleteInProgressItem(
+                                  widget.ongoingList[index]);
+                            }),
+                        TextButton(
+                            child: const Text("CANCEL"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            })
+                      ],
+                    );
+                  });
+            },
+          );
         });
   }
 
