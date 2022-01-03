340358375ae7819fbb921f60dfbfb82e890725ee
Lixing <lixing.sun@thoughtworks.com>
2022-01-03 12:09:27 +0800

    Add weather in header

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:dellery_app/pages/home/radio.dart';
 import 'package:dellery_app/pages/home/parkinglot.dart';
@@ -19,11 +24,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double temperature = 0;
+  String weather = '';
+  String weatherIcon = '01d';
+
   @override
   void initState() {
     super.initState();
 
     widget.localStorage.readStore();
+    getWeather();
+  }
+
+  void getWeather() async {
+    await dotenv.load(fileName: ".env");
+    var url = Uri.parse(
+        'http://api.openweathermap.org/data/2.5/weather?q=Chengdu,cn&APPID=${dotenv.env['WEATHER_API_APP_ID']}&units=metric');
+    var response = await http.get(url);
+
+    stderr.writeln('Response body: ${response.body}');
+    setState(() {
+      final weatherData = json.decode(response.body);
+      temperature = weatherData['main']['temp'];
+      weather = weatherData['weather'][0]['main'];
+      weatherIcon = weatherData['weather'][0]['icon'];
+    });
   }
 
   @override
@@ -34,7 +59,26 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green[900],
-        title: Text(today),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(today),
+            Row(
+              children: [
+                Text('${temperature.round()}°C, $weather'),
+                Padding(
+                  padding: const EdgeInsets.only(left: 6),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.green[300],
+                    backgroundImage:
+                        AssetImage('assets/weather/$weatherIcon@2x.png'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
       body: Container(
           constraints: const BoxConstraints.expand(),
