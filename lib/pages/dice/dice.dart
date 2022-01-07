5282476ff39d72c28d5156649ad77621138cdddb
Lixing <slxtrjh325@gmail.com>
2022-01-07 20:34:14 +0800

    Enable adding option for dice

--- lib/pages/dice/dice.dart
+++ lib/pages/dice/dice.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:dellery_app/components/button.dart';
 import 'package:flutter/material.dart';
 
 class DicePage extends StatefulWidget {
@@ -12,7 +13,8 @@ class DicePage extends StatefulWidget {
 const defaultDiceValue = "?";
 
 class _DicePageState extends State<DicePage> {
-  final List<String> options = ["AAAAAAA", "BBBBBBB", "CCCCCCC", "DDDDDDD", "EEEEEEE", "FFFFFFF", "GGGGGGG", "HHHHHH"];
+  final addController = TextEditingController();
+  final List<String> options = [];
 
   String currentValue = defaultDiceValue;
 
@@ -20,18 +22,22 @@ class _DicePageState extends State<DicePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Dice")),
-        body: Center(
-            child: Card(
-                child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 36, horizontal:60),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: GestureDetector(
+        body: Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/dice_bg.jpg"),
+                    fit: BoxFit.cover)),
+            child: Center(
+                child: Card(
+                    child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 60),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
                     child: CircleAvatar(
                       radius: 100,
                       backgroundColor: Colors.amber,
@@ -41,28 +47,62 @@ class _DicePageState extends State<DicePage> {
                       ),
                     ),
                     onTap: () {
-                      setState(() {
-                        currentValue =
-                            options[Random().nextInt(options.length)];
-                      });
+                      if (options.isNotEmpty) {
+                        setState(() {
+                          currentValue =
+                              options[Random().nextInt(options.length)];
+                        });
+                      }
                     },
-                  )),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: Center(
-                    child: Wrap(
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 20,
-                  runSpacing: 20,
-                  children: options
-                      .map((name) => Chip(
-                            label: Text(name),
-                          ))
-                      .toList(),
-                )),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 30),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: Center(
+                            child: Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          spacing: 20,
+                          runSpacing: 20,
+                          children: options
+                              .map((name) => Chip(
+                                    label: Text(name),
+                                  ))
+                              .toList(),
+                        )),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        child: TextFormField(
+                          controller: addController,
+                          decoration: const InputDecoration(
+                            labelText: 'New Item',
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter the name',
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: CustomTextButton(
+                          child: const Text("ADD"),
+                          onPressed: () {
+                            setState(() {
+                              options.add(addController.text);
+                              addController.text = '';
+                            });
+                          },
+                          isPrimary: true,
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
-            ],
-          ),
-        ))));
+            )))));
   }
 }
